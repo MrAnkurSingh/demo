5b22290b11b8c4bab801ae098fcb53bcdb56e55c
MrAnkurSingh <ankurkumarsingh952398@gmail.com>
2025-12-25 18:15:39 +0530

    filler

--- xyz.js
+++ xyz.js
@@ -9,7 +9,7 @@ async function getData(){
     const response=await fetch('https://dog.ceo/api/breeds/image/random')
     const data= await response.json();
 
-    const img = document.createElement('img');
+    const img = document.createElement('img')
     img.src = data.message;
    
     img.style.width = '400px';
